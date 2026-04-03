32a78fb68697fc475be85c40ee92c2c0b0abdb71
Miguel Molina <miguel217995@gmail.com>
2026-04-03 08:54:42 -0400

    Test the new branch

--- src/graph/state.py
+++ src/graph/state.py
@@ -2,6 +2,8 @@ from typing import Annotated, TypedDict, List, Dict, Optional
 from langgraph.graph.message import add_messages
 from operator import add
 
+print("State module loaded successfully.")
+
 # Matches CorePositioning in snapshot_chain.py
 class PositioningCore(TypedDict):
     practice_model: str  
